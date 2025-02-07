b0d8b261fc1e17a28e765934a9f6d9359b5c55c7
mainguyen <nguyenthingocmai193.com>
2025-02-07 16:02:37 +0700

    full on air

---
+++ Source/arts-core-main/arts-core/Extensions/OrderExtension.cs
@@ -0,0 +1,35 @@
+﻿using arts_core.Models;
+
+namespace arts_core.Extensions
+{
+    public static class OrderExtension
+    {
+        public static float CalculateTotalPrice(this List<Order> orders)
+        {
+            return (float)orders.Sum(o => o.TotalPrice);
+        }
+        public static List<VariantsCount> GetVariantsCountOfOrders(this List<Order> orders)
+        {
+            var variantscount = orders
+              .GroupBy(o => o.VariantId)
+              .Select(g => new VariantsCount
+              {
+                  VariantId = g.Key,
+                  VariantCode = g.Key.ToString(),
+                  Count = g.Count(),
+                  TotalQuanity = g.Sum(v => v.Quanity)
+              })
+              .OrderByDescending(g => g.Count)
+              .ToList();
+            return variantscount;
+        }
+    }
+
+    public class VariantsCount
+    {
+        public int VariantId { get; set; }
+        public int Count { get; set; }
+        public string VariantCode { get; set; } = string.Empty;
+        public int TotalQuanity { get; set; }
+    }
+}
---
+++ Source/arts-core-main/arts-core/ReturnModels/GroupedResult.cs
@@ -0,0 +1,8 @@
+﻿namespace arts_core.ReturnModels
+{
+    public class GroupedResult
+    {
+        public DateTime Date { get; set; } 
+        public int Value { get; set; }
+    }
+}
